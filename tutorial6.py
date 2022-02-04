d53f35ccf722964529e5c9d3efc606af869d30a0
S2ATech <98845575+S2ATech@users.noreply.github.com>
2022-02-04 18:18:40 +0100

    Update tutorial6.py

--- tutorial6.py
+++ tutorial6.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
-import os
 
+import os
 import telegram
 
 app = Flask(__name__)
@@ -8,30 +8,16 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 
 def webhook():
-
     bot = telegram.Bot(token=os.environ["YOURAPIKEY"])
-
-
     if request.method == "POST":
-
         update = telegram.Update.de_json(request.get_json(force=True), bot)
-
         chat_id     = update.effective_chat.id
-
         text        = update.message.text
-
         first_name  = update.effective_chat.first_name
-
         # Reply with the same message
-
         bot.sendMessage(chat_id=chat_id, text=f"{text} {first_name}")
-        
-        dispatcher = bot.dispatcher
-
-  return 'ok'
-
+        return 'ok'
     return 'error'
 
 def index():
-
     return webhook()
