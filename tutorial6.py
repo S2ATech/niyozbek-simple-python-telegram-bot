6206c4928b559abee5bc198d47f21b9c60e25a48
S2ATech <98845575+S2ATech@users.noreply.github.com>
2022-02-04 14:53:51 +0100

    Update tutorial6.py

--- tutorial6.py
+++ tutorial6.py
@@ -8,7 +8,41 @@
 #https://seminar.io/2018/09/03/building-serverless-telegram-bot/
 #https://www.heroku.com/
 from flask import Flask, render_template, request
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update, InlineKeyboardMarkup
 
+from bson.json_util import dumps
+
+from multicolorcaptcha import CaptchaGenerator
+
+from jokes import getJoke
+
+from telegram.ext import (
+
+    Updater,
+
+    CommandHandler,
+
+    MessageHandler,
+
+    Filters,
+
+    ConversationHandler,
+
+    CallbackContext,
+
+    PicklePersistence,
+
+)
+
+from telegram.utils import helpersimport telegram
+
+import pymongo
+
+import logging
+
+import os
+
+import pickle
 import os
 import telegram
 
@@ -25,6 +59,7 @@ def webhook():
         first_name  = update.effective_chat.first_name
         # Reply with the same message
         bot.sendMessage(chat_id=chat_id, text=f"{text} {first_name}")
+        update.message.reply_text(JOINED.replace("REPLACEME", url), reply_markup=ReplyKeyboardMarkup(reply_keyboard))
         return 'ok'
     return 'error'
 
